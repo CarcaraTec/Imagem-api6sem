5b7535aebdd372a8ca2bd882e8cf21bfbd7446c8
Alan Araujo Rodrigues <88983038+alantrs@users.noreply.github.com>
2024-06-13 20:36:45 -0300

    feature/IM-140

--- db/LGPD_SCHEMA.sql
+++ db/LGPD_SCHEMA.sql
@@ -1,5 +1,4 @@
-CREATE SCHEMA LGPD;
-USE LGPD;
+use imagem;
 
 CREATE TABLE TERMO(
                    ID_TERMO   INT AUTO_INCREMENT PRIMARY KEY   ,
@@ -9,7 +8,6 @@ CREATE TABLE TERMO(
                    DH_CRIACAO DATETIME     DEFAULT CURRENT_TIMESTAMP
                   );
 
-
 CREATE TABLE ITEM (
                     ID_ITEM       INT AUTO_INCREMENT PRIMARY KEY,
                     ID_TERMO      INT          NOT NULL         ,
@@ -18,18 +16,10 @@ CREATE TABLE ITEM (
                     FOREIGN KEY(ID_TERMO) REFERENCES TERMO(ID_TERMO)
                    );
 
-CREATE TABLE USUARIO(
-                     ID_USUARIO INT AUTO_INCREMENT PRIMARY KEY,
-                     DS_NOME    VARCHAR(255) NOT NULL         ,
-                     DS_EMAIL   VARCHAR(255) NOT NULL         ,
-                     DS_CPF     VARCHAR(255)       UNIQUE     ,
-                     DS_STATUS  VARCHAR(255) NOT NULL
-                    );
-
 CREATE TABLE ITENS_USUARIO(
                            ID_ITEM_USUARIO INT AUTO_INCREMENT PRIMARY KEY  ,
                            ID_ITEM         INT NOT NULL                    ,
-                           ID_USUARIO      INT NOT NULL                    ,
+                           USER_ID      INT NOT NULL                    ,
                            FOREIGN KEY(ID_ITEM)    REFERENCES ITEM(ID_ITEM),
-                           FOREIGN KEY(ID_USUARIO) REFERENCES USUARIO(ID_USUARIO)
+                           FOREIGN KEY(USER_ID) REFERENCES USER(USER_ID)
                           );
